292b8d77e5a0549d994524d6f33d5c79379b76a5
emregurbuz <emre_gurbuz@yahoo.com>
2022-05-31 01:21:59 +0300

    anasayfa yapıldı

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,24 +1,52 @@
-import { Text, View,Image } from 'react-native'
+import { Text, View, Image, ActivityIndicator, Linking, TouchableOpacity, ScrollView } from 'react-native'
 import React, { Component } from 'react'
 import axios from 'axios'
 import { SafeAreaView } from 'react-native-safe-area-context'
 export class App extends Component {
-  state={
-    ABOUT:[]
+  state = {
+    ABOUT: [],
+    loading: true
   }
-  async getAbout(){
+  async getAbout() {
     response = await axios.get("https://mynode.emregurbuz.net/ABOUT")
-    console.log("gelen response "+JSON.stringify(response.data,null,2))
+    this.setState({ ABOUT: response.data[0], loading: false })
+
   }
-  componentDidMount() { 
+  componentDidMount() {
     this.getAbout()
-   }
+  }
   render() {
-    return (
-      <SafeAreaView>
-        <Text>App</Text>
-      </SafeAreaView>
-    )
+    if (this.state.loading) {
+      return (
+        <View style={{ flex: 1, alignSelf: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator style={{ transform: [{ scale: 2 }] }} size={'large'} color={'black'} />
+          <Text style={{ marginTop: '20%', fontWeight: '600', fontSize: 20 }}>Bilgiler Getiriliyor..</Text>
+        </View>
+      )
+    }
+    else {
+
+      return (
+        <ScrollView style={{ flex: 1 }}>
+          <SafeAreaView style={{ padding: 10, flex: 1 }}>
+            <Image style={{ height: 200, width: 200, borderRadius: 20, resizeMode: 'contain', alignSelf: 'center' }} source={{ uri: this.state.ABOUT?.IMAGE_URI }} />
+            <Text style={{ alignSelf: 'center', marginVertical: 10, fontSize: 17, fontWeight: '600' }}>{this.state.ABOUT?.NAME + " " + this.state.ABOUT?.SURNAME} </Text>
+            <View style={{ alignSelf: 'center' }}>
+              <TouchableOpacity onPress={() => Linking.openURL(this.state.ABOUT?.GITHUB)} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image style={{ width: 50, height: 50, resizeMode: 'contain' }} source={require('../assets/github.png')} />
+                <Text>{this.state.ABOUT.GITHUB}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => Linking.openURL(this.state.ABOUT?.LINKEDIN)} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Image style={{ width: 50, height: 50, resizeMode: 'contain' }} source={require('../assets/linkedin.png')} />
+                <Text>{this.state.ABOUT.LINKEDIN}</Text>
+              </TouchableOpacity>
+            </View>
+            <Text style={{ textDecorationLine: 'underline', fontWeight: '600', fontSize: 17, marginVertical: 3, alignSelf: 'center' }}>HAKKIMDA</Text>
+            <Text style={{}}>  {this.state.ABOUT?.BIO} </Text>
+          </SafeAreaView>
+        </ScrollView>
+      )
+    }
   }
 }
 
